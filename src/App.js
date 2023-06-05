1646d6d4374e1117a9435dd76bb256e9678e0a9b
Lambertn33 <lambertgithub@gmail.com>
2023-06-05 19:31:19 +0200

    feat: show/hide cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const handleShowCart = () => {
+    setCartVisible(true);
+  }
+
+  const handleHideCart = () => {
+    setCartVisible(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      {cartVisible && <Cart onHideCart={handleHideCart}/>}
+      <Header onShowCart={handleShowCart}/>
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from './Cart.module.css';
 import Modal from "../UI/Modal";
 
-export default function Cart() {
+export default function Cart({ onHideCart }) {
   const cartItems = (
     <ul className={classes['cart-items']}>
       {[{ id: "1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
@@ -17,7 +17,7 @@ export default function Cart() {
         <span>30.00</span>
     </div>
     <div className={classes.actions}>
-        <button className={classes['button--alt']}>close</button>
+        <button className={classes['button--alt']} onClick={onHideCart}>close</button>
         <button className={classes.button}>Order</button>
     </div>
   </Modal>;
